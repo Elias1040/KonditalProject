2e2f7c45612069a2066ff5976af78d9980ca400e
Elias Nielsen Andersen <elia1040@elevcampus.dk>
2022-05-23 10:37:57 +0200

    indlæsning

--- KonditalProject/Program.cs
+++ KonditalProject/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿int hvilepuls, maxpuls;
+
+Console.WriteLine("hvilepuls:");
+while (!int.TryParse(Console.ReadLine(), out hvilepuls))
+{
+    Console.WriteLine("Ugyldig værdi!");
+    Thread.Sleep(1500);
+    Console.Clear();
+    Console.WriteLine("hvilepuls:");
+}
+Console.Clear();
+
+Console.WriteLine("maxpuls:");
+while (!int.TryParse(Console.ReadLine(), out maxpuls))
+{
+    Console.WriteLine("Ugyldig værdi!");
+    Thread.Sleep(1500);
+    Console.Clear();
+    Console.WriteLine("maxpuls:");
+}
+Console.Clear();
